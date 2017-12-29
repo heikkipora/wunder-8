20a2b76315087d941cfcf6141dac097c3ed99bc8
Heikki Pora <heikki.pora@reaktor.fi>
2017-12-29 20:59:41 +0200

    A working rust version. pow!

--- wunder-rust/src/main.rs
+++ wunder-rust/src/main.rs
@@ -6,6 +6,9 @@ use std::io::Error;
 use std::io::prelude::*;
 use std::io::Read;
 
+const LINE_MAX_LENGTH: usize = 80;
+const SPACE_LENGTH: usize = 1;
+
 fn main() {
     let mut text = String::new();
     load_text("../alastalon_salissa.txt", &mut text);
@@ -13,10 +16,45 @@ fn main() {
     let mut words_by_length: BTreeMap<usize, VecDeque<String>> = BTreeMap::new();
     group_words_by_length(&text, &mut words_by_length);
 
-    for (length, words) in &words_by_length {
-        println!("{}: {}", length, words.get(0).unwrap());
-    }
+    let text = collect_optimal_rows(&mut words_by_length);
     write_text("../alastalon_salissa_output.txt", &text);
+}
+
+fn collect_optimal_rows(words_by_length: &mut BTreeMap<usize, VecDeque<String>>) -> String {
+  let mut rows: Vec<String> = Vec::new();
+  loop {
+      match collect_optimal_row(words_by_length) {
+          Some(row) => rows.push(row),
+          None => break
+      }
+  }
+  return rows.join("\n");
+}
+
+fn collect_optimal_row(words_by_length: &mut BTreeMap<usize, VecDeque<String>>) -> Option<String> {
+    let mut row: Vec<String> = Vec::new();
+    let mut line_length: usize = 0;
+    while line_length < LINE_MAX_LENGTH {
+        match longest_word(LINE_MAX_LENGTH - line_length, words_by_length) {
+            Some(word) => {
+                line_length += word.chars().count() + SPACE_LENGTH;
+                row.push(word);
+            },
+            None => break
+        }
+    }
+    if row.len() > 0 {
+        return Some(row.join(" "));
+    }
+    return None;
+}
+
+fn longest_word(max_length: usize, words_by_length: &mut BTreeMap<usize, VecDeque<String>>) -> Option<String> {
+    words_by_length
+        .iter_mut()
+        .rev()
+        .find(|&(&length, ref words)| length <= max_length && words.iter().count() > 0)
+        .map(|(_, words)| words.pop_front().unwrap())
 }
 
 fn group_words_by_length(text: &String, words_by_length: &mut BTreeMap<usize, VecDeque<String>>) {
@@ -30,7 +68,10 @@ fn add_to_map(word: String, words_by_length: &mut BTreeMap<usize, VecDeque<Strin
     if !words_by_length.contains_key(&length) {
         words_by_length.insert(length, VecDeque::new());
     }
-    words_by_length.get_mut(&length).unwrap().push_back(word)
+    words_by_length
+        .get_mut(&length)
+        .unwrap()
+        .push_back(word)
 }
 
 fn load_text(file_name: &str, text: &mut String) -> Result<(), Error> {
